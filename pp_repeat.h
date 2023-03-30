016cb0ebe921aa21b4687ece24efdd8fde60e2ee
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-03-30 05:51:12 +0200

    pp_repeat.h modified

--- pp_repeat.h
+++ pp_repeat.h
@@ -15,7 +15,6 @@
 #define	PP_REPEAT(x,...)	PP_IF(PP_NUM(x))(PP_EVAL(PP_REPEAT_(x,__VA_ARGS__)),PP_REPEAT(x,##__VA_ARGS__))
 #define	PP_REPEAT_(x,y,...)	PP_IF(PP_FALSE(x))(,PP_OBSTRUCT(PP_REPEAT__)()(PP_DEC(x),y,##__VA_ARGS__)PP_OBSTRUCT(y)(PP_DEC(x),##__VA_ARGS__))
 #define PP_REPEAT__()		PP_REPEAT_
-
 PP_TEST("x \"hello world\" (0)\"hello world\" (1)\"hello world\" (2)y"	, x PP_REPEAT(3,"hello world")y)
 PP_TEST("x PP_REPEAT(N,a,b)y"	, x PP_REPEAT(N,a,b)y)
 PP_TEST("x a (0,b)a (1,b)y"	, x PP_REPEAT(2,a,b)y)
@@ -41,4 +40,13 @@ PP_TEST("x 0y"		, x PP_HAVE2ARGS(x,)y)
 PP_TEST("x 1y"		, x PP_HAVE2ARGS(x,y)y)
 PP_TEST("x 1y"		, x PP_HAVE2ARGS(x,,z)y)
 PP_TEST("x 1+2+3y"	, x PP_REDUCE(PP_HAVE2ARGS,PP_SUM,1,2,3)y)
+
+#define	PP_COUNT(x,y,...)	PP_INC(x),##__VA_ARGS__
+#define	PP_NARGS(...)		PP_IF(PP_HAVEARGS(__VA_ARGS__))(PP_REDUCE_(PP_HAVE2ARGS,PP_COUNT,0,##__VA_ARGS__,X),1)
+PP_TEST("0"	, PP_NARGS())
+PP_TEST("1"	, PP_NARGS(x))
+PP_TEST("2"	, PP_NARGS(x,y))
+PP_TEST("3"	, PP_NARGS(,,))
+PP_TEST("4"	, PP_NARGS(a,,,))
+PP_TEST("5"	, PP_NARGS(a,,,,z))
 
